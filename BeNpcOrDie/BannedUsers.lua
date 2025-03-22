f7e7d50153ad498068e4354aa1e69b7302b9b48c
Blockier <86944635+DespiseBlockier@users.noreply.github.com>
2025-03-22 03:26:16 +0000

    Update and rename BannedUsers.lua to FinalBannedUsers.lua

--- BeNpcOrDie/BannedUsers.lua
+++
@@ -1,4 +0,0 @@
-local table = {
-    ["F8A15701-DBDC-4BE5-84C3-0D917B711023"] = "Banned by owner of Game, dm @_r0cks for a chance to apeal",
-}
-return table
---
+++ BeNpcOrDie/FinalBannedUsers.lua
@@ -0,0 +1,4 @@
+local table = {
+    ["8D8E13E0-D73F-4B37-87CA-380175BFC376"] = "Banned by owner of Game, dm @_r0cks for a chance to apeal",
+}
+return table
