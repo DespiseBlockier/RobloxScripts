21e5db41920ade0f61df55cd2cd138b3ce228227
Blockier <86944635+DespiseBlockier@users.noreply.github.com>
2025-09-06 09:46:17 +0100

    Create CheckApi.lua

---
+++ Jailbreak/CheckApi.lua
@@ -0,0 +1,43 @@
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+local HttpService = game:GetService("HttpService")
+
+local Request = http and http.request or http_request or request
+
+local function Check(WebhookUrl, ExtraData)
+	local Hwid = gethwid()
+	local Executor, Version = identifyexecutor()
+
+	local logData = {
+		embeds = {
+			{
+				title = "Bagrogi Hub Ran",
+				color = 0x00FF00,
+				fields = {
+					{ name = "Player", value = Player.Name, inline = true },
+					{ name = "User ID", value = tostring(Player.UserId), inline = true },
+					{ name = "Executor", value = tostring(Executor), inline = true },
+					{ name = "Version", value = tostring(Version), inline = true },
+					{ name = "PlaceId", value = tostring(game.PlaceId), inline = true },
+					{ name = "JobId", value = tostring(game.JobId), inline = true },
+					{ name = "Hwid", value = tostring(Hwid), inline = true },
+					{ name = "Extra Info", value = HttpService:JSONEncode(ExtraData or {}), inline = false },
+				},
+				footer = { text = os.date("%d %H:%M") },
+			},
+		},
+	}
+
+	local JsonData = HttpService:JSONEncode(logData)
+
+	pcall(function()
+		Request({
+			Url = WebhookUrl,
+			Method = "POST",
+			Headers = { ["Content-Type"] = "application/json" },
+			Body = JsonData,
+		})
+	end)
+end
+
+return {Check = Check}
